b183ce1b65a2884d85dce95041cb865750d0f47b
wilkhu90 <sumeet.wilkhu@gmail.com>
2015-07-29 15:28:06 -0500

    Added few changes to Simulator.

--- DesignPattern/src/edu/auburn/szw0069/FactoryPattern/PizzaSimulator.java
+++ DesignPattern/src/edu/auburn/szw0069/FactoryPattern/PizzaSimulator.java
@@ -5,7 +5,7 @@ public class PizzaSimulator {
 	public static void main(String[] args) {
 		
 		PizzaStore nyStore = new NYPizzaStore();
-		nyStore.orderPizza("Single");
+		nyStore.orderPizza("T");
 		
 
 	}
